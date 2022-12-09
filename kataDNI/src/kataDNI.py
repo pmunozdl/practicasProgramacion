3a8eeae0ae017cbb62cc4961a8d250e0baea1d34
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MacBook-Pro-de-Pablo.local>
2022-12-09 13:27:06 +0100

    intento de agrupación, falta corregir

--- kataDNI/src/kataDNI.py
+++ kataDNI/src/kataDNI.py
@@ -1,4 +1,4 @@
-
+'''
 def longitud_dni(dni):
     longitud = len(dni)
     if longitud == 9:
@@ -20,12 +20,6 @@ def longitud_dni(dni):
             return ValueError 
     else:
         return ValueError
-'''
-def dniSuma(dni):
-    letras = []
-    respuesta = sum(letras[0:8])
-    return respuesta
-'''
 
 def dniLetra(dni):
     letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J", "Z","S","Q","V","H","L","C","K","E"]
@@ -43,19 +37,101 @@ def dniLetra(dni):
 
 def nie(dni):
         lista = list(dni)
-        if lista[0] == "0":
-            lista[0] = "X"
+        if lista[0] == "X":
+            lista[0] = "0"
             nie_valido = ''.join(lista)
             return nie_valido
-        elif lista[0] == "1":
-            lista[0] = "Y"
+        elif lista[0] == "Y":
+            lista[0] = "1"
             nie_valido = ''.join(lista)
             return nie_valido
-        elif lista[0] == "2":
-            lista[0] = "Z"
+        elif lista[0] == "Z":
+            lista[0] = "2"
             nie_valido = ''.join(lista)
             return nie_valido
         else:
             return dni
+'''
 
+def dni_valido(dni):
+    longitud = len(dni)
+    lista = list(dni)
+    letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J", "Z","S","Q","V","H","L","C","K","E"]
+    if longitud == 9:
+        if dni[0:8].isdigit(): #caso para dni normal
+            if dni[8].isdigit() == False:
+                if dni[8] == "U" or dni[8] == "I" or dni[8] == "Ñ" or dni[8] == "O":
+                    return ValueError
+                else:
+                    x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
+        #falta hacer la suma para dni[0:8]
+                    ans = x % 23
+                    if (dni[8]) == letras[ans]:
+                        return True
+                    else:
+                        return ValueError
+            else:
+                return ValueError
+        elif dni[0] == "X" and dni[1:8].isdigit():
+            lista[0] = "0"
+            nie_valido = ''.join(lista)
+            if nie_valido[0:8].isdigit():
+                if nie_valido[8].isdigit() == False:
+                    if nie_valido[8] == "U" or nie_valido[8] == "I" or nie_valido[8] == "Ñ" or nie_valido[8] == "O":
+                        return ValueError
+                    else:
+                        x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
+                        #falta hacer la suma para dni[0:8]
+                        ans = x % 23
+                        if (dni[8]) == letras[ans]:
+                            return True
+                        else:
+                            return ValueError
+                else:
+                    return ValueError
+            else:
+                    return ValueError
+        elif dni[0] == "Y" and dni[1:8].isdigit():
+            lista[0] = "1"
+            nie_valido = ''.join(lista)
+            if nie_valido[0:8].isdigit():
+                if nie_valido[8].isdigit() == False:
+                    if nie_valido[8] == "U" or nie_valido[8] == "I" or nie_valido[8] == "Ñ" or nie_valido[8] == "O":
+                        return ValueError
+                    else:
+                        x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
+                        #falta hacer la suma para dni[0:8]
+                        ans = x % 23
+                        if (dni[8]) == letras[ans]:
+                            return True
+                        else:
+                            return ValueError
+                else:
+                    return ValueError
+            else:
+                return ValueError
+        elif dni[0] == "Z" and dni[1:8].isdigit():
+            lista[0] = "2"
+            nie_valido = ''.join(lista)
+            if nie_valido[0:8].isdigit():
+                if nie_valido[8].isdigit() == False:
+                    if nie_valido[8] == "U" or nie_valido[8] == "I" or nie_valido[8] == "Ñ" or nie_valido[8] == "O":
+                        return ValueError
+                    else:
+                        x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
+                        #falta hacer la suma para dni[0:8]
+                        ans = x % 23
+                        if (dni[8]) == letras[ans]:
+                            return True
+                        else:
+                            return ValueError
+                else:
+                    return ValueError
+            else:
+                return ValueError      
+        else:
+            return ValueError
+
+    else:
+        return ValueError
 
--- kataDNI/tests/test_kataDNI.py
+++ kataDNI/tests/test_kataDNI.py
@@ -1,79 +1,78 @@
 import unittest
 import src.kataDNI
-from src.kataDNI import longitud_dni
-from src.kataDNI import dniLetra
-from src.kataDNI import nie 
+from src.kataDNI import dni_valido
+
 
 class test_kataDNI(unittest.TestCase):
     #1
     def test_9_cifras(self):
         dni = "191919191" 
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #2
     def test_9_cifras_y_letras(self):
         dni = "F98I01Q11"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #3
     def test_8_cifras(self): 
         dni = "F98I01Q1"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #4
     def test_dni_valido(self):
-        dni = "12345678P"
-        longitud = longitud_dni(dni)
-        self.assertEqual(9,longitud)
+        dni = "12345678J"
+        longitud = dni_valido(dni)
+        self.assertEqual(True,longitud)
     #5
     def test_dni_letra_invalida_O(self):
         dni = "12345678O"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #6
     def test_dni_letra_invalida_I(self):
         dni = "12345678I"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #7
     def test_dni_letra_invalida_Ñ(self):
         dni = "12345678Ñ"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #8
     def test_dni_letra_invalida_U(self):
         dni = "12345678U"
-        longitud = longitud_dni(dni)
+        longitud = dni_valido(dni)
         self.assertEqual(ValueError,longitud)
     #9
     def test_letra_buena(self):
         dni = "00000000T"
-        bueno = dniLetra(dni)
+        bueno = dni_valido(dni)
         self.assertEqual(True,bueno)
     #10
     def test_letra_erronea(self):
         dni = "00000001P"
-        bueno = dniLetra(dni)
+        bueno = dni_valido(dni)
         self.assertEqual(ValueError,bueno)
     #11
     def test_nie_valido_0(self):
-        dni = "00000000T"
-        bueno = nie(dni)
-        self.assertEqual("X0000000T",bueno)
+        dni = "X0000000T"
+        bueno = dni_valido(dni)
+        self.assertEqual(True,bueno)
      #12
     def test_nie_valido_1(self):
-        dni = "10000000R"
-        bueno = nie(dni)
-        self.assertEqual("Y0000000R",bueno)
+        dni = "Y0000000R"
+        bueno = dni_valido(dni)
+        self.assertEqual(True,bueno)
      #13
     def test_nie_valido_2(self):
-        dni = "20000000W"
-        bueno = nie(dni)
-        self.assertEqual("Z0000000W",bueno)
+        dni = "Z0000000W"
+        bueno = dni_valido(dni)
+        self.assertEqual(True,bueno)
     def test_nie_caso_especial(self):
-        dni = "01200000A"
-        bueno = nie(dni)
-        self.assertEqual("X1200000A",bueno)
+        dni = "X1200000A"
+        bueno = dni_valido(dni)
+        self.assertEqual(True,bueno)
         
    
     
