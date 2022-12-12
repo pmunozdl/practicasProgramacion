67eff311bc474f5cc57f5988303569881b8e2118
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MacBook-Pro-de-Pablo.local>
2022-12-12 09:36:58 +0100

    test con constructor

--- kataDNI/src/kataDNI.py
+++ kataDNI/src/kataDNI.py
@@ -1,57 +1,58 @@
-'''
-def longitud_dni(dni):
-    longitud = len(dni)
-    if longitud == 9:
-        if dni[0:8].isdigit():
-            if dni[8].isdigit() == False:
-                if (dni[8] == "U"):
-                    return ValueError
-                elif (dni[8] == "I"):
-                    return ValueError
-                elif (dni[8] == "O"):
-                    return ValueError
-                elif (dni[8] == "Ñ"):
-                    return ValueError
-                else:
-                    return longitud
-            else:
-                return ValueError
-        else: 
-            return ValueError 
-    else:
-        return ValueError
 
-def dniLetra(dni):
-    letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J", "Z","S","Q","V","H","L","C","K","E"]
-    rango = dni[0:8]
-    if rango.isdigit():
-        x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
-        #falta hacer la suma para dni[0:8]
-        ans = x % 23
-        if (dni[8]) == letras[ans]:
-            return True
+class kataDNI:
+    def longitud_dni(self, dni):
+        longitud = len(dni)
+        if longitud == 9:
+            if dni[0:8].isdigit():
+                if dni[8].isdigit() == False:
+                    if (dni[8] == "U"):
+                        return ValueError
+                    elif (dni[8] == "I"):
+                        return ValueError
+                    elif (dni[8] == "O"):
+                        return ValueError
+                    elif (dni[8] == "Ñ"):
+                        return ValueError
+                    else:
+                        return longitud
+                else:
+                    return ValueError
+            else: 
+                return ValueError 
         else:
             return ValueError
-    else:
-        return ValueError
 
-def nie(dni):
-        lista = list(dni)
-        if lista[0] == "X":
-            lista[0] = "0"
-            nie_valido = ''.join(lista)
-            return nie_valido
-        elif lista[0] == "Y":
-            lista[0] = "1"
-            nie_valido = ''.join(lista)
-            return nie_valido
-        elif lista[0] == "Z":
-            lista[0] = "2"
-            nie_valido = ''.join(lista)
-            return nie_valido
+    def dniLetra(self,dni):
+        letras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J", "Z","S","Q","V","H","L","C","K","E"]
+        rango = dni[0:8]
+        if rango.isdigit():
+            x = int(dni[0]) + int(dni[1]) + int(dni[2]) + int(dni[3]) + int(dni[4]) + int(dni[5]) + int(dni[6]) + int(dni[7]) 
+            #falta hacer la suma para dni[0:8]
+            ans = x % 23
+            if (dni[8]) == letras[ans]:
+                return True
+            else:
+                return ValueError
         else:
-            return dni
-'''
+            return ValueError
+
+    def nie(self,dni):
+            lista = list(dni)
+            if lista[0] == "X":
+                lista[0] = "0"
+                nie_valido = ''.join(lista)
+                return nie_valido
+            elif lista[0] == "Y":
+                lista[0] = "1"
+                nie_valido = ''.join(lista)
+                return nie_valido
+            elif lista[0] == "Z":
+                lista[0] = "2"
+                nie_valido = ''.join(lista)
+                return nie_valido
+            else:
+                return dni
+    '''
 
 def dni_valido(dni):
     longitud = len(dni)
@@ -134,4 +135,4 @@ def dni_valido(dni):
 
     else:
         return ValueError
-
+'''
--- kataDNI/tests/test_kataDNI.py
+++ kataDNI/tests/test_kataDNI.py
@@ -1,9 +1,80 @@
 import unittest
-import src.kataDNI
-from src.kataDNI import dni_valido
+from src.kataDNI import kataDNI
 
 
 class test_kataDNI(unittest.TestCase):
+    #1
+    def test_9_cifras(self):
+        dni = "191919191"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #2
+    def test_9_cifras_y_letras(self):
+        dni = "F98I01Q11"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #3
+    def test_8_cifras(self): 
+        dni = "F98I01Q1"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #4
+    def test_kataDNI(self):
+        dni = "12345678J"
+        longitud = kataDNI()
+        self.assertEqual(9,longitud.longitud_dni(dni))
+    #5
+    def test_dni_letra_invalida_O(self):
+        dni = "12345678O"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #6
+    def test_dni_letra_invalida_I(self):
+        dni = "12345678I"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #7
+    def test_dni_letra_invalida_Ñ(self):
+        dni = "12345678Ñ"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #8
+    def test_dni_letra_invalida_U(self):
+        dni = "12345678U"
+        longitud = kataDNI()
+        self.assertEqual(ValueError,longitud.longitud_dni(dni))
+    #9
+    def test_letra_buena(self):
+        dni = "00000000T"
+        bueno = kataDNI()
+        self.assertEqual(True,bueno.dniLetra(dni))
+    #10
+    def test_letra_erronea(self):
+        dni = "00000001P"
+        bueno = kataDNI()
+        self.assertEqual(ValueError,bueno.dniLetra(dni))
+    #11
+    def test_nie_valido_0(self):
+        dni = "X0000000T"
+        bueno = kataDNI()
+        self.assertEqual("00000000T",bueno.nie(dni))
+     #12
+    def test_nie_valido_1(self):
+        dni = "Y0000000R"
+        bueno = kataDNI()
+        self.assertEqual("10000000R",bueno.nie(dni))
+     #13
+    def test_nie_valido_2(self):
+        dni = "Z0000000W"
+        bueno = kataDNI()
+        self.assertEqual("20000000W",bueno.nie(dni))
+    #14
+    def test_nie_caso_especial(self):
+        dni = "X1200000A"
+        bueno = kataDNI()
+        self.assertEqual("01200000A",bueno.nie(dni))
+'''
+ #test para métodos sin constructor
     #1
     def test_9_cifras(self):
         dni = "191919191" 
@@ -73,7 +144,7 @@ class test_kataDNI(unittest.TestCase):
         dni = "X1200000A"
         bueno = dni_valido(dni)
         self.assertEqual(True,bueno)
-        
+ '''       
    
     
     
