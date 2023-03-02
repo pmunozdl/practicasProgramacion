a042d7ee1decbadf30433cd90714ca79ce660adc
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MacBook-Pro-de-Pablo.local>
2023-03-02 19:30:34 +0100

    modifico test

--- katabolos/katabolos_con_tuplas/tedede-main/Bolos/katabolos.py
+++ katabolos/katabolos_con_tuplas/tedede-main/Bolos/katabolos.py
@@ -1,21 +1,21 @@
 class Bolos:
 
-    def calcularScore(self, partida):
-        print(list(enumerate(partida)))
+    def bowling_score(self, frames):
+        print(list(enumerate(frames)))
         acc = 0
-        for index, x in list(enumerate(partida)):
+        for index, x in list(enumerate(frames)):
             if x[0] == 10:
-                if partida[index + 1][0] == 10:
-                    tmp = partida[index + 1][0]
-                    tmp2 = partida[index + 2][0]
+                if frames[index + 1][0] == 10:
+                    tmp = frames[index + 1][0]
+                    tmp2 = frames[index + 2][0]
                     acc += x[0] + tmp + tmp2
                 else:
-                    tmp = sum(partida[index + 1])
+                    tmp = sum(frames[index + 1])
                     acc += x[0] + tmp
                     print(acc)
             else:
-                if sum(x) == 10:
-                    tmp = partida[index + 1][0]
+                if int(sum(x)) == 10:
+                    tmp = frames[index + 1][0]
                     acc += sum(x) + tmp
                 else:
                     acc += sum(x)
