f2e7f8e285f431e433399a607ed80f28d85ffd54
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MacBook-Pro-de-Pablo.local>
2023-03-16 13:11:32 +0100

    kata avanzada

--- katabolos/katabolos/src/katabolos/kataBolos.py
+++ katabolos/katabolos/src/katabolos/kataBolos.py
@@ -1,95 +1,67 @@
 class kataBolos:
 
-    # def bowling_score(self, frames):
-    #     score = 0
-    #     index = 0
-    #     rolls = 0
-    #     while index < len(frames) and rolls < 20:
-    #         frames = frames.replace(" ", "")
-    #         frame_score= frames[index]
-    #         if frame_score == "/":
-    #             score += self.spare_score(frames, index)
-    #         elif frame_score == 'X':
-    #             rolls = rolls + 1
-    #             if index < len(frames) - 2:
-    #                 score += self.strike_score(frames, index)
-    #         elif frame_score != '-':
-    #             score += int(frame_score)
-    #         index = index + 1
-    #         rolls = rolls + 1
-    #     return score
-    
-    # def get_frames(self, frames):
-    #     self.frames = frames
-
-    # def spare_score(self, frame_scores, index):
-    #     if index < 18:
-    #         return 10 - self.frame_value(frame_scores[index - 1]) + self.frame_value(frame_scores[index + 1])
-    #     elif index == 19:
-    #         return 10 - self.frame_value(frame_scores[index - 1])
-
-    # def strike_score(self,frame_scores, index):
-    #     if frame_scores[index + 2] == '/':
-    #         return 20
-    #     elif index == 18:  
-    #         return 10 + self.frame_value(frame_scores[index + 1])
-    #     elif index == 19:
-    #         return 10
-    #     elif index < 18:
-    #         return 10 + self.frame_value(frame_scores[index + 1]) + self.frame_value(frame_scores[index + 2])
-
-    # def frame_value(self, frame):
-    #     if frame == 'X':
-    #         return 10
-    #     if frame == '-':
-    #         return 0
-    #     return int(frame)
-
     def bowling_score (self, frames):
         score = 0
         frames = frames.replace(" ", "")
-        size = len(frames)
-        for e in frames: 
-                if e == "/": #falta el semipleno al final. Falla la resta de la tirada de delante
-                    posicion = frames.index("/")
-                    if posicion < 19:
-                        score += 10 + self.frame_value(frames[posicion + 1]) - self.frame_value(frames[posicion - 1])
-                    elif posicion == 19:
-                        if posicion + 1 =="X":
+        frames = frames.replace("-", "0")
+        lista = self.numeroRondas(frames) #cargo la función para obtener la lista de las diez tiradas
+        print(lista)
+        if self.calcularRonda(frames) == 10: #tiene que haber 10 rondas
+            for e in range(len(lista)):
+                if lista[e][1] == "/": #falta el semipleno al final. Falla la resta de la tirada de delante
+                    if e in range(len(lista[0:9])):
+                        score += 10 + self.frame_value(lista[e + 1][0])
+                        print("puntuacion semipleno", score)
+                        print(self.frame_value(lista[e + 1][0]))
+                    elif e in lista[9]:
+                        if lista[9][1] == "/":
+                            if lista[9][2] =="X":
+                                score += 20
+                                print("puntuación semipleno y luego pleno",score)
+                            elif (lista[9][2]).isdigit() == True:
+                                score += 10 - self.frame_value(lista[9][0]) + self.frame_value(lista[9][2])
+                                print("semipleno y tirada normal",score)
+                            # else: 
+                            #     score += 10 - self.frame_value(lista[9][0])
+                            #     print("semipleno en ulti", score)
+                elif lista[e] == "X":
+                    if e in range(len(lista[0:9])):
+                    #     if posicion + 1 == "0" and posicion + 2 == "X":
+                    #             score += 10 + self.frame_value(frames[posicion + 1]) + self.frame_value(frames[posicion + 2])
+                    #     elif self.calcularRonda(frames) <= 9:
+                    #         score += 10 + self.frame_value(frames[posicion + 1]) + self.frame_value(frames[posicion + 2]) 
+                        if lista[e+1][2] == "/":
                             score += 20
-                        else: 
-                            score += 10 - self.frame_value(frames[posicion- 1])
-                    frames = frames[posicion+1:]
-                elif e == "X":
-                    print(frames)
-                    posicion = frames.index("X")
-                    print(frames)
-                    if posicion + 1 == "0" and posicion + 2 == "X":
-                            score += 10 + self.frame_value(frames[posicion + 1]) + self.frame_value(frames[posicion + 2])
-                    elif posicion < 19:
-                        score += 10 + self.frame_value(frames[posicion + 1]) + self.frame_value(frames[posicion + 2]) 
-                    elif posicion > 18:
-                        if posicion + 2 == "/":
-                            score += 20
-                        elif posicion + 1 == "X":
-                            score += 10
-                            if posicion + 2 =="X":
-                                score += 10
+                            print("pleno y después semipleno",score)
                         else:
-                            score += 10
-                    frames = frames[posicion+1:]
-                elif e == "-":
-                    score = score
-                else: 
-                   score += int(e) #suma los valores si no hay pleno, o semipleno
-        print (score)
-        return score
+                            score += 10 + self.frame_value(lista [e+1]) + self.frame_value(lista[e+2])
+                            print("pleno normal",score)
+                    elif e in lista[9]:
+                        if lista[9][0] == "X":
+                            if lista [9][2] == "/":
+                                score += 20
+                                print("pleno y semipleno en ultima ronda",score)
+                            elif lista [9][1] == "X":
+                                score += 10
+                                if lista [9] [2] =="X":
+                                    score += 10
+                                    print("pleno pleno pleno",score)
+                                else:
+                                    score += 10 + self.frame_value(lista[9][1]) + self.frame_value(lista[9][2])# no se si hay que sumar las posiciones 1 y 2
+                                    print("pleno pleno y casi",score)
+                elif lista[e].isdigit() == True: #digito
+                        score += int(lista[e][0])+ int (lista[e][1])  #suma los valores si no hay pleno, o semipleno
+                        print("tirada normal",score)
+            print("puntuación total",score)
+            return score
         
     def frame_value(self, frame):
         if frame == 'X':
             return 10
-        if frame == '-':
+        elif frame == '-':
             return 0
+        elif frame.isdigit() == True:
+             return int(frame)
         return int(frame)
     def get_frames(self, frames):
         self.frames = frames
@@ -97,30 +69,58 @@ class kataBolos:
     def calcularRonda(self, frames):
         numerorondas = 0 #tiene que llegar a 10
         tiradas = 0
+        frames = frames.replace(" ", "")
+        framess = iter(frames)
+        resultado = []
         for e in range(0,len(frames)): #inicio, fin y paso
-            print("lista",frames)
-            print("elemento", frames[e])
-            if numerorondas < 10:
-                print("posicion", e)
                 if str(frames[e]) == "X":
                     numerorondas += 1
+                    resultado.append(frames[e])
                 elif str(frames[e]) == "/" or str(frames[e]) == "-" or str(e).isdigit() == True:
                     if e <= (len(frames)-1):
                         tiradas += 1
-                        print("tiradas", tiradas)
                         if tiradas % 2 == 0:
                             numerorondas += 1
-                        elif str(frames[e+1]) == "X":
+                        elif str(next(framess)) == "X":
                              tiradas = tiradas - 1
-                             numerorondas += 0 
+                             numerorondas += 0
                 else:
                      raise ValueError("no vale")
-                print("numero de rondas",numerorondas)    
-            elif numerorondas == 10:
-                pass
-            else:
-                raise ValueError("solo puede haber 10 rondas")
         return(numerorondas)
+
+    def numeroRondas(self, frames):
+        numerorondas = 0 #tiene que llegar a 10
+        tiradas = 0
+        frames = frames.replace(" ", "")
+        framess = iter(frames)
+        lista = []
+
+        for e in range(0,len(frames)): #inicio, fin y paso
+                if str(frames[e]) == "X":
+                    numerorondas += 1
+                    lista.append(frames[e])
+                    lista.append(",")
+                elif str(frames[e]) == "/" or str(frames[e]) == "-" or str(e).isdigit() == True:
+                    if e <= (len(frames)-1):
+                        tiradas += 1
+                        lista.append(frames[e])
+                        
+                        if tiradas % 2 == 0:
+                            numerorondas += 1
+                            lista.append(",")
+                        elif str(next(framess)) == "X":
+                             tiradas = tiradas - 1
+                             numerorondas += 0
+                else:
+                     raise ValueError("no vale")
+        listaComas = []
+        for e in range(len(lista)):
+             if lista[e] == ",":
+                  listaComas.append(e)
+        lista.pop(listaComas[-1])
+        string = ''.join(lista)
+        final = string.split(",", 10)
+        return(final)
     #def restricciones(self, frames):
 #hacer un método de calcular ronda, que me permita saber en qué ronda estoy 
 #bowling_score("XXX429")
@@ -129,3 +129,7 @@ class kataBolos:
 #semiplenos solo en posiciones impares. 
 #codigo para test
 # ponerlo todo en distintas funciones, quitar los self. 
+
+
+#tengo que conseguir convertir el string en una lista de las distintas tiradas. Una vez tenga la lista, podré
+#acceder a las distintas tiradas. Clase prueba
--- katabolos/katabolos/tests/test_kata_bolos.py
+++ katabolos/katabolos/tests/test_kata_bolos.py
@@ -15,55 +15,55 @@ class test_kata_bolos(unittest.TestCase):
         score = kataBolos()
         score.get_frames(frames)
         self.assertEqual(score.bowling_score(frames), resultado_ok)
-    def notest_all_zeros(self):
+    def test_all_zeros(self):
         frames = "--------------------"
         self.escenario_partida(frames, 0)
 
-    def notest_all_ones(self):
+    def test_all_ones(self):
         frames = "11 111111111111111111"
         self.escenario_partida(frames, 20)
 
-    def notest_a_spare(self): 
+    def test_a_spare(self): 
         frames = "1/111111111111111111"
         self.escenario_partida(frames, 29)
 
-    def notest_two_spare(self): # dos semipleno
+    def test_two_spare(self): # dos semipleno
         frames = "1/11111/111111111111"
         self.escenario_partida(frames, 38)
 
-    def notest_a_strike(self): #un pleno
+    def test_a_strike(self): #un pleno
         frames = "X111111111111111111-" #no cuenta un 1 porque se anula con el pleno
         self.escenario_partida(frames,30)
 
-    def notest_a_spare_then_strike(self): #semipleno y luego pleno
+    def test_a_spare_then_strike(self): #semipleno y luego pleno
         frames = "1/X1111111111111111-" #no cuenta un 1 porque se anula con el pleno
         self.escenario_partida(frames, 48)
 
-    def notest_three_strikes(self): #tres plenos
-        frames = "XXX11111111111111---" #no cuenta los 3 últimos 0 porque se anulan con el pleno
+    def test_three_strikes(self): #tres plenos
+        frames = "XXX11111111111111" #no cuenta los 3 últimos 0 porque se anulan con el pleno
         self.escenario_partida(frames,30+21+12+14)
 
-    def notest_a_spare_with_zero(self): #semipleno
+    def test_a_spare_with_zero(self): #semipleno
         frames = "1/-11111111111111111"
         self.escenario_partida(frames,27)
         
-    def notest_perfect_game(self):
+    def test_perfect_game(self):
         frames = "XXXXXXXXXXXX" #añadimos dos más por las tiradas extras con plenos 
         self.escenario_partida(frames,300)
         
-    def notest_spare_at_end(self):
+    def test_spare_at_end(self):
         frames = "------------------5/4" 
         self.escenario_partida(frames,14)
 
-    def notest_strike_at_end(self): ## único que falla
+    def test_strike_at_end(self): ## único que falla
         frames = "------------------X-"
         self.escenario_partida(frames,10)
 
-    def notest_almost_perfect_game(self):
+    def test_almost_perfect_game(self):
         frames = "XXXXXXXXXX9/" #añado dos más por las normas
         self.escenario_partida(frames,289)
     
-    def notest_codewars_uno(self):
+    def test_codewars_uno(self):
         frames = "5/ 4/ 3/ 2/ 1/ 0/ X 9/ 4/ 8/8" #falla en el semipleno del final
         self.escenario_partida(frames,150)
     
@@ -77,4 +77,9 @@ class test_kata_bolos(unittest.TestCase):
         score = kataBolos()
         score.get_frames(frames)
         self.assertEqual(score.calcularRonda(frames), 10)
+    def test_3_calcular_ronda(self):
+        frames = "1111111111111111111" #19 son 9 rondas, porque la 10 no está completa. 
+        score = kataBolos()
+        score.get_frames(frames)
+        self.assertEqual(score.calcularRonda(frames), 9)
     
